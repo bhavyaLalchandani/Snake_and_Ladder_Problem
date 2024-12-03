c6afc0e5be09fbacd9793be41315c856162c9d30
bhavyaLalchandani <bhavyal321@gmail.com>
2024-12-03 22:48:09 +0530

    Welcome Message added

---
+++ src/SnakeLadderGame.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class SnakeLadderGame {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Snake and Ladder Game!");
+    }
+}
